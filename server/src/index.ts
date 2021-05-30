a7a50a98214ac7b2afccb337ae57b8b33d7da82c
lachaka <lachomladenov@gmail.com>
2021-05-30 14:26:43 +0300

    Add environment variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is listening on port ${SERVER_PORT}`);
 });
